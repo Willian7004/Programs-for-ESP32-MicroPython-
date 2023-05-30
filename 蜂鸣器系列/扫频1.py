186883c5fb1cdc9483a5b3424fc45ef8637b0ff3
Willian7004 <128359604+Willian7004@users.noreply.github.com>
2023-05-30 07:46:04 +0000

    Pending changes exported from your codespace

---
+++ 蜂鸣器系列/扫频1.py
@@ -0,0 +1,18 @@
+#导入Pin模块
+from machine import Pin
+import time
+
+#定义蜂鸣器控制对象
+beep=Pin(25,Pin.OUT)   
+    
+#程序入口
+if __name__=="__main__":
+    i=0
+    a=1
+    while True:
+        i=not i  #非运算
+        beep.value(i)  
+        time.sleep_ms(a)  #脉冲频率为2KHz
+        a+=1
+        if a==25 :
+            a=0
