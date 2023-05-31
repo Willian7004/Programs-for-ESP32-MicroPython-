79a270e5ad5b42ec3d418c3c5a57632c93ac1fc0
Willian7004 <128359604+Willian7004@users.noreply.github.com>
2023-05-31 23:19:07 +0800

    Add files via upload

---
+++ RGB灯随机颜色.py
@@ -0,0 +1,26 @@
+#接线： WS-->(16)         
+
+#导入Pin模块
+from machine import Pin
+from neopixel import NeoPixel
+import time
+import random
+
+#定义RGB控制对象
+#控制引脚为16，RGB灯串联5个
+pin=16
+rgb_num=5
+rgb_led=NeoPixel(Pin(pin,Pin.OUT),rgb_num)  
+
+#程序入口
+if __name__=="__main__" :
+
+    while True:        
+            for i in range(rgb_num):
+                a=random.randint(0,255)
+                b=random.randint(0,255)
+                c=random.randint(0,255)
+                rgb_led[i]=(a, b, c)
+                rgb_led.write()
+                time.sleep_ms(100)
+            
